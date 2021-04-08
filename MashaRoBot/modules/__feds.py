f10037cac3b6a8bfc07cecd35d740802efc04b21
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2021-04-08 21:53:06 +0530

    Update __feds.py

--- MashaRoBot/modules/__feds.py
+++ MashaRoBot/modules/__feds.py
@@ -916,6 +916,7 @@ Ah, group management. It's all fun and games, until you start getting spammers i
 But then you have multiple groups, and you don't want these spammers in any of your groups - how can you deal? Do you have to ban them manually, in all your groups?
 No more! With federations, you can make a ban in one chat overlap to all your other chats.
 You can even appoint federation admins, so that your trustworthiest admins can ban across all the chats that you want to protect.
+For improved fed system you can use @HaritaRobot.
 *Commands:*\n
 - /newfed <fedname>: Creates a new federation with the given name. Only one federation per user. Max 64 chars name allowed.
 - /delfed: Deletes your federation, and any information related to it. Will not unban any banned users.
